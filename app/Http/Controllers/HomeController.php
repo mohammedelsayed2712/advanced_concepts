4dee31d1590c7731439a909044c24261e005b130
mohammedelsayed2712 <mohammedelsayed2712@gmail.com>
2025-04-13 00:09:58 +0200

    access current route

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,6 +10,16 @@ class HomeController extends Controller
      */
     public function __invoke(Request $request)
     {
+        // return view('welcome');
+
+        // dd($request->route());
+        // dd($request->route()->getName());
+        // dd($request->route()->getParameters());
+        // dd($request->route()->getAction());
+        // dd($request->route()->getController());
+        // dd(Route::current());
+        // dd(Route::current()->getName());
+        // dd(Route::currentRouteAction());
         return view('welcome');
     }
 }
